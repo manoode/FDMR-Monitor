aebb88b126e46b74ebbb6a71078429c640224496
Manuel Hesse <manoode@users.noreply.github.com>
2023-12-23 16:41:53 +0100

    Update navbar.php

--- html/include/navbar.php
+++ html/include/navbar.php
@@ -1,9 +1,9 @@
 <nav class="main-header navbar navbar-expand-lg navbar-light navbar-dark text-sm">
     <div class="container text-nowrap">
         <a href="#" class="navbar-brand">
-            <img src="img/Logo_mini.png" alt="FreeDMR" class="brand-image img-circle elevation-3"
+            <img src="img/Logo_mini.png" alt="FriendlyNet" class="brand-image img-circle elevation-3"
                 style="opacity: .8">
-            <span class="brand-text font-weight-light">FreeDMR</span>
+            <span class="brand-text font-weight-light">FriendlyNet</span>
         </a>
         <button class="navbar-toggler order-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
             aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
